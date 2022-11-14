6599a8ca8ed7c53af9fc57d124e3de2fe77d68a7
saipepu <saipepu.mdy257@gmail.com>
2022-11-14 22:41:48 +0700

    first commit

---
+++ dist/index.de5c0784.js
@@ -0,0 +1,3 @@
+console.log("script is running");
+
+//# sourceMappingURL=index.de5c0784.js.map
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+console.log('script is running')
+
+
+console.log(_.concat([1234,23],[2,42]))
